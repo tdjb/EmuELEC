ec618b9c24d064b2038ef5ad6052da84a920f0fc
Johannes Brandstätter <jbrandst@2ds.eu>
2021-07-01 09:39:01 +0200

    Update enet to v1.3.12.

--- packages/addons/addon-depends/enet/package.mk
+++ packages/addons/addon-depends/enet/package.mk
@@ -2,8 +2,7 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="enet"
-PKG_VERSION="e33ca1d"
-PKG_SHA256="0ba5547de2c4c7fc79d367179a9bc92a7ac27e9258dd50fb277cd8761afaf9b0"
+PKG_VERSION="v1.3.12"
 PKG_LICENSE=""
 PKG_SITE="https://github.com/cgutman/enet/"
 PKG_URL="https://github.com/cgutman/enet/archive/$PKG_VERSION.tar.gz"
